7abef89babff99112800d1ca3defe068ca9db4e2
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-06-16 18:25:30 +0600

    remove try() and catch() and added error status showing package

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
+require('express-async-errors');
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const error = require("./middlewares/error.js");
 
 const userRouter = require('./api/routers/userRouter');
+
 
 
 app.use(express.json());
@@ -11,6 +14,7 @@ app.use(cors());
 
 app.use('/api/user/', userRouter)
 
+app.use(error);
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
